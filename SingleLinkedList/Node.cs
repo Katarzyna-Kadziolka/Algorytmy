b7d8d3d01a2114e909e0a6408be1bf3fdbf43d33
Katarzyna <kasiakadziolka@gmail.com>
2022-10-30 17:18:43 +0100

    First part complete

---
+++ SingleLinkedList/Node.cs
@@ -0,0 +1,22 @@
+﻿namespace SingleLinkedList; 
+
+public class Node<T>
+{    
+    public T Data {get; set;}
+    public Node<T> Next {get; set;}
+    public Node(T data, Node<T> next = null) {
+        Data = data; Next = next;
+    }
+    public override string ToString() => (this==null)? "null" : $"{Data} -> ";
+    
+    public static void PrintSingleLinkedList<T>( Node<T> head ) {
+        var node = head;
+        Console.Write("head -> ");
+        while (node != null) {
+           Console.Write(node.ToString());
+           node = node.Next;
+        }
+        Console.Write("null");
+        
+    }
+}
---
+++ SingleLinkedList/Program.cs
@@ -0,0 +1,7 @@
+﻿using SingleLinkedList;
+
+Node<int> head1 = 
+    new Node<int>(2,
+        new Node<int>(5,
+            new Node<int>(1)));
+Node<int>.PrintSingleLinkedList(head1);
