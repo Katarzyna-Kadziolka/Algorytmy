5598e5b6c3adbd195e6c96e6e3cee291050cce37
Katarzyna <kasiakadziolka@gmail.com>
2022-11-12 10:48:59 +0100

    Start multiset

---
+++ Multiset/MultiSet.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+
+namespace Multiset; 
+
+public class MultiSet<T>: IMultiSet<T> {
+    private Dictionary<T, int> _dictionary;
+
+    public MultiSet() {
+        _dictionary = new Dictionary<T, int>();
+    }
+    
+    
+    public IEnumerator<T> GetEnumerator() {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() {
+        return GetEnumerator();
+    }
+
+    public void Add(T item) {
+        throw new NotImplementedException();
+    }
+
+    public void Clear() {
+        throw new NotImplementedException();
+    }
+
+    public bool Contains(T item) {
+        throw new NotImplementedException();
+    }
+
+    public void CopyTo(T[] array, int arrayIndex) {
+        throw new NotImplementedException();
+    }
+
+    public bool Remove(T item) {
+        throw new NotImplementedException();
+    }
+
+    public int Count { get; }
+    public bool IsReadOnly { get; }
+    public IMultiSet<T> Add(T item, int numberOfItems = 1) {
+        throw new NotImplementedException();
+    }
+
+    public IMultiSet<T> Remove(T item, int numberOfItems = 1) {
+        throw new NotImplementedException();
+    }
+
+    public IMultiSet<T> RemoveAll(T item) {
+        throw new NotImplementedException();
+    }
+
+    public IMultiSet<T> UnionWith(IEnumerable<T> other) {
+        throw new NotImplementedException();
+    }
+
+    public IMultiSet<T> IntersectWith(IEnumerable<T> other) {
+        throw new NotImplementedException();
+    }
+
+    public IMultiSet<T> ExceptWith(IEnumerable<T> other) {
+        throw new NotImplementedException();
+    }
+
+    public IMultiSet<T> SymmetricExceptWith(IEnumerable<T> other) {
+        throw new NotImplementedException();
+    }
+
+    public bool IsSubsetOf(IEnumerable<T> other) {
+        throw new NotImplementedException();
+    }
+
+    public bool IsProperSubsetOf(IEnumerable<T> other) {
+        throw new NotImplementedException();
+    }
+
+    public bool IsSupersetOf(IEnumerable<T> other) {
+        throw new NotImplementedException();
+    }
+
+    public bool IsProperSupersetOf(IEnumerable<T> other) {
+        throw new NotImplementedException();
+    }
+
+    public bool Overlaps(IEnumerable<T> other) {
+        throw new NotImplementedException();
+    }
+
+    public bool MultiSetEquals(IEnumerable<T> other) {
+        throw new NotImplementedException();
+    }
+
+    public bool IsEmpty { get; }
+    public IEqualityComparer<T> Comparer { get; } = null!;
+
+    public int this[T item] => throw new NotImplementedException();
+
+    public IReadOnlyDictionary<T, int> AsDictionary() {
+        throw new NotImplementedException();
+    }
+
+    public IReadOnlySet<T> AsSet() {
+        throw new NotImplementedException();
+    }
+}
--- ThorVsFrostGiants/Program.cs
+++ ThorVsFrostGiants/Program.cs
@@ -5,19 +5,37 @@ var giants = Array.ConvertAll(Console.ReadLine()?.Split(" ", StringSplitOptions.
                               ?? Array.Empty<string>(), int.Parse);
 var shift = 0;
 for (int k = 0; k < quantum; k++) {
+    var powerMultiplier = 1;
     for (int i = 0; i < giants.Length; i++) {
-        var powerMultiplier = 1;
-        for (int j = i; j < shift + i + 1; j++) {
-            if (j >= giants.Length) {
+        if (i == 0 && shift == 0) {
+            for (int j = 0; j < giants.Length; j++) {
+                if (giants[j] % quantum == 0) {
+                    Console.WriteLine($"{j} {j}");
+                    return;
+                }
+            }
+        }
+        for (int j = i; j < shift + i; j++) {
+            if (j >= giants.Length || j + shift >= giants.Length) {
                 break;
             }
-            powerMultiplier *= giants[j];
+
+            if (j == 0) {
+                for (int l = j; l < shift + 1; l++) {
+                    powerMultiplier *= giants[l];
+                }
+            }
+            else {
+                powerMultiplier = (powerMultiplier / (giants[j - 1]) * giants[j + shift]);
+
+            }
         }
 
         if (powerMultiplier % quantum == 0) {
             Console.WriteLine($"{i} {i + shift}");
             return;
         }
+        
     }
 
     shift++;
