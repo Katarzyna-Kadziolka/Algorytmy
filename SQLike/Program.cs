7731383138770feb57790a76b0bd0b516abaca14
Katarzyna <kasiakadziolka@gmail.com>
2023-01-09 17:24:07 +0100

    3/4 good

---
+++ SQLike/Program.cs
@@ -0,0 +1,31 @@
+﻿using SQLike;
+
+//Console.WriteLine("Adam".SQLike("A%a"));
+Console.WriteLine("Agnieszka".SQLike("A%a")); //true
+Console.WriteLine("agnieszka".SQLike("A%a"));
+Console.WriteLine("Agnieszka".SQLike("%_a")); //true
+Console.WriteLine("a".SQLike("%_a"));
+Console.WriteLine("alab".SQLike("%"));
+
+Console.WriteLine();
+
+Console.WriteLine("".SQLike(""));
+Console.WriteLine("a".SQLike(""));
+Console.WriteLine("".SQLike("a"));
+Console.WriteLine("".SQLike("_"));
+Console.WriteLine("".SQLike("%"));
+
+Console.WriteLine();
+
+Console.WriteLine("a".SQLike("_"));
+Console.WriteLine("aa".SQLike("_"));
+Console.WriteLine("a".SQLike("%"));
+Console.WriteLine("aa".SQLike("%"));
+Console.WriteLine("a".SQLike("_%"));
+Console.WriteLine("aa".SQLike("_%")); 
+Console.WriteLine("aaa".SQLike("_%"));
+Console.WriteLine("".SQLike("_%"));
+Console.WriteLine("a".SQLike("%_"));
+ Console.WriteLine("aa".SQLike("%_"));
+ Console.WriteLine("aaa".SQLike("%_"));
+ Console.WriteLine("".SQLike("%_"));
---
+++ SQLike/StringExtensions.cs
@@ -0,0 +1,47 @@
+﻿namespace SQLike;
+
+public static class StringExtensions {
+    public static bool SQLike(this string text, string pattern) {
+        if (pattern == string.Empty && text.Length > 0) {
+            return false;
+        }
+
+        if (text.Length == 0) {
+            foreach (var sign in pattern) {
+                if (sign != '%') return false;
+            }
+
+            return true;
+        }
+
+        var textIndex = 0;
+        for (int patternIndex = 0; patternIndex < pattern.Length; patternIndex++) {
+            if (pattern[patternIndex] == '%') {
+                var numberToMoveTextIndex = text.Length - pattern.Length + patternIndex + 1;
+                if (numberToMoveTextIndex < 0) {
+                    return false;
+                }
+                for (int i = textIndex; i < numberToMoveTextIndex; i++) {
+                    textIndex++;
+                }
+            }
+
+            else if (pattern[patternIndex] == '_') {
+                textIndex++;
+                if (textIndex < text.Length && patternIndex + 1 == pattern.Length) return false;
+            }
+            else {
+                if (pattern[patternIndex] != text[textIndex]) {
+                    return false;
+                }
+
+                textIndex++;
+                if (textIndex < text.Length && patternIndex + 1 == pattern.Length) return false;
+            }
+        }
+
+        return true;
+
+    }
+}
+
